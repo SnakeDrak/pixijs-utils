7bfba49fe7eda0453b475c12ec258634cc5f279d
SnakeDrak <snake@talitasia.com>
2014-08-28 13:43:29 +0200

    Add interactive mode

--- pixi-utils.js
+++ pixi-utils.js
@@ -9,18 +9,28 @@
  * @param int radius
  * @param int Xcenter
  * @param int Ycenter
+ * @param bool interactive Set hit area?
  **/
 (function() {
-    PIXI.Graphics.prototype.rectPolygon = function(numberOfSides, radius, Xcenter, Ycenter) {
+    PIXI.Graphics.prototype.rectPolygon = function(numberOfSides, radius, Xcenter, Ycenter, interactive) {
 
-        this.moveTo(Math.round(Xcenter + radius * Math.sin(0)), Math.round(Ycenter + radius * Math.cos(0)));
+        interactive = interactive || false;
+        this.moveTo(Xcenter + radius * Math.sin(0), Ycenter + radius * Math.cos(0));
 
+        var points = [];
         for (var i = 1; i <= numberOfSides; i += 1) {
             var coord = {
-                x: Math.round(Xcenter + radius * Math.sin(i * 2 * Math.PI / numberOfSides)),
-                y: Math.round(Ycenter + radius * Math.cos(i * 2 * Math.PI / numberOfSides))
+                x: Xcenter + radius * Math.sin(i * 2 * Math.PI / numberOfSides),
+                y: Ycenter + radius * Math.cos(i * 2 * Math.PI / numberOfSides)
             };
+            if (interactive)
+                points.push(new PIXI.Point(coord.x, coord.y));
             this.lineTo(coord.x, coord.y);
+        }
+
+        if (interactive) {
+            this.interactive = true;
+            this.hitArea = new PIXI.Polygon(points);
         }
 
         return this;
